c2142ecb57b7259964224dcc12b7f0eadbdd0687
sirmantris <sandro.calzada@gmail.com>
2017-05-29 16:58:34 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,11 @@
+house1 <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?") ##read data
+house2 <-rbind(house1[house1$Date=="1/2/2007",],house1[house1$Date=="2/2/2007",])
+house2$Date <- as.Date(house2$Date,"%d/%m/%Y") ##date formatting
+house_final<-cbind(house2, "Datetime" = as.POSIXct(paste(house2$Date, house2$Time))) 
+
+
 plot(house_final$Global_active_power ~ house_final$DateTime, type="l", xlab= "", ylab="Global Active power (kilowatts)") ##plot graph 2
+
+
+dev.copy(png, file="plot2.png", height=640, width=640)
+dev.off()
