0a57afa1270bdaef62de4289fc9581464bc4d49c
sirmantris <sandro.calzada@gmail.com>
2017-05-29 12:28:38 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,5 @@
+with(house_final, {plot(Sub_metering_1 ~ DateTime, type="l", xlab= "", ylab="Energy Sub Metering")}) ##graph plot 3
+lines(house_final$Sub_metering_2 ~ house_final$DateTime, col = 'Red')
+lines(house_final$Sub_metering_3 ~ house_final$DateTime, col = 'Blue')
+legend("topright", lty=1, lwd =3, col=c("black","red","blue") ,
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
