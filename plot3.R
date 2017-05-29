9457842e32ab22701de19900b47fe46db7e3ab91
sirmantris <sandro.calzada@gmail.com>
2017-05-29 16:59:54 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,3 +3,8 @@ lines(house_final$Sub_metering_2 ~ house_final$DateTime, col = 'Red')
 lines(house_final$Sub_metering_3 ~ house_final$DateTime, col = 'Blue')
 legend("topright", lty=1, lwd =3, col=c("black","red","blue") ,
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+
+dev.copy(png, file="plot3.png", height=640, width=640) ##save file
+dev.off() 
+
